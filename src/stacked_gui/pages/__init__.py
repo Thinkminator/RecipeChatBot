b386bee418f907ba77c2d4f907c9f84d6aae8e4e
adc1252247 <adc1252247@gmail.com>
2025-09-24 21:39:52 +0800

    Add files via upload
    Page format with back button

---
+++ src/stacked_gui/pages/__init__.py
@@ -0,0 +1,14 @@
+
+from .base_page import BasePage
+from .mode_selection_page import ModeSelectionPage
+from .input_type_page import InputTypePage
+from .text_page import TextPage
+from .speech_page import SpeechPage
+
+__all__ = [
+    "BasePage",
+    "ModeSelectionPage",
+    "InputTypePage",
+    "TextPage",
+    "SpeechPage",
+]
